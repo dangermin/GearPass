a23377db878aa5d51ab9200ad422c3512f5ad4d6
c-mac88 <christianmcfarland@gmail.com>
2016-06-20 00:00:33 -0700

    added ion-google-places

--- Frontend/www/js/app.js
+++ Frontend/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngCordova', 'ngCordovaOauth'])
+angular.module('starter', ['ionic', 'ion-google-place', 'starter.controllers', 'starter.services', 'ngCordova', 'ngCordovaOauth'])
 
 .run(function($ionicPlatform, APP_CONFIG) {
 
